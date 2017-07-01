adeea469c23a731e92f299a98dffd7c899510124
mojinchuan <mojinchuan@126.com>
2017-07-01 21:06:41 +0800

    加以改进

--- updiff-mvn-plugin/src/test/java/com/sand/updiff/mvnplugin/JGitTest.java
+++ updiff-mvn-plugin/src/test/java/com/sand/updiff/mvnplugin/JGitTest.java
@@ -1,5 +1,16 @@
 package com.sand.updiff.mvnplugin;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
@@ -10,21 +21,12 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.junit.Test;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.List;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
-
 /**
  * Created by sun on 2015/8/2.
  */
 public class JGitTest{
-
+	//此目录下需要有.git目录
+	static final String PROJECT_PATH="e:/workspaces/workspaceTest/updiff/";
 
 	@Test
 	public void test_call() throws IOException, GitAPIException {
@@ -32,7 +34,7 @@ public class JGitTest{
 		FileRepositoryBuilder builder = new FileRepositoryBuilder();
 
 
-		Repository repository = builder.setGitDir(new File("e:/workspaces/workspaceTest/updiff/.git"))
+		Repository repository = builder.setGitDir(new File(PROJECT_PATH+".git"))
 				.readEnvironment()
 				.findGitDir()
 				.build();
@@ -53,13 +55,14 @@ public class JGitTest{
 				.setNewTree(newTreeIterator)
 				.setOldTree(oldTreeIterator)
 				.call();
-		String[] files = new String[diffs.size()];
-		int i=0;
+		List<String> files = new ArrayList<String>();
 		for (DiffEntry diffEntry: diffs){
 			System.out.println(diffEntry.getNewPath());
-			files[i]=diffEntry.getNewPath();
-			files[i]=files[i].replace("src/main/java", "target/classes").replace("src/main/resources", "target/classes").replace(".java", ".class");
-			i++;
+			if(diffEntry.getNewPath().indexOf("/src/test/java/")!=-1)
+				continue;
+			String filePath=diffEntry.getNewPath();
+			filePath=filePath.replace("src/main/java", "target/classes").replace("src/main/resources", "target/classes").replace(".java", ".class");
+			files.add(filePath);
 		}
 		
 		zip(files);
@@ -67,29 +70,24 @@ public class JGitTest{
 	}
 	
 	static final int BUFFER = 2048;
-	public static void zip (String[] files) {
+	public static void zip (List<String> files) {
 	      try {
 	         BufferedInputStream origin = null;
 	         FileOutputStream dest = new 
-	           FileOutputStream("e:/workspaces/workspaceTest/updiff/updiff.zip");
+	           FileOutputStream(PROJECT_PATH+"/updiff.zip");
 	         ZipOutputStream out = new ZipOutputStream(new 
 	           BufferedOutputStream(dest));
 	         //out.setMethod(ZipOutputStream.DEFLATED);
 	         byte data[] = new byte[BUFFER];
-	         // get a list of files from current directory
-	         File f = new File("e:/workspaces/workspaceTest/updiff/");
-	         
-	         for (int i=0; i<files.length; i++) {
-	            System.out.println("Adding: "+files[i]);
-	            if(files[i].equals(".settings"))
-	            	continue;
+	         for (int i=0; i<files.size(); i++) {
+	            System.out.println("Adding: "+files.get(i));
 	            FileInputStream fi = new 
-	              FileInputStream(f.getAbsolutePath()+"/"+files[i]);
+	              FileInputStream(PROJECT_PATH+files.get(i));
 	            origin = new 
 	              BufferedInputStream(fi, BUFFER);
-	            files[i]=files[i].replace("/target/", "/WEB-INF/");
+	            String filePath=files.get(i).replace("/target/", "/WEB-INF/");
 	            
-	            ZipEntry entry = new ZipEntry(files[i]);
+	            ZipEntry entry = new ZipEntry(filePath);
 	            out.putNextEntry(entry);
 	            int count;
 	            while((count = origin.read(data, 0, 
@@ -104,4 +102,12 @@ public class JGitTest{
 	      }
 	   }
 
+	/**TODO 
+	 * 	1.更加易使用,配置或者传参数。或者maven插件。
+		2.如果更新的文件位于  packaging为jar或者maven-plugin的模块中，则不把此文件放入zip包中，
+	而对于有文件更新的jar或者maven-plugin模块做标记，最后把这此JAR放入zip的WEB-INF/lib中。
+		3.对于test类不打包
+	*/
+		
+	
 }
