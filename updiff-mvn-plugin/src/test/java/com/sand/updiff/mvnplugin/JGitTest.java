6d09a574b7fcc6bb02d9390fe9214c6c1d4cfd2e
mojinchuan <mojinchuan@126.com>
2017-07-01 20:32:24 +0800

    Add packaging to zip in gitTest. Act as "git diff HEAD 232ad233 --name-only| xargs zip update.zip"

--- updiff-mvn-plugin/src/test/java/com/sand/updiff/mvnplugin/JGitTest.java
+++ updiff-mvn-plugin/src/test/java/com/sand/updiff/mvnplugin/JGitTest.java
@@ -10,9 +10,15 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
 import org.junit.Test;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 /**
  * Created by sun on 2015/8/2.
@@ -26,7 +32,7 @@ public class JGitTest{
 		FileRepositoryBuilder builder = new FileRepositoryBuilder();
 
 
-		Repository repository = builder.setGitDir(new File("D:\\0005_git\\updiff\\.git"))
+		Repository repository = builder.setGitDir(new File("e:/workspaces/workspaceTest/updiff/.git"))
 				.readEnvironment()
 				.findGitDir()
 				.build();
@@ -40,17 +46,62 @@ public class JGitTest{
 		newTreeIterator.reset(reader, newTree);
 
 		CanonicalTreeParser oldTreeIterator = new CanonicalTreeParser();
-		ObjectId oldTree = git.getRepository().resolve("8b5adeb^{tree}");
+		ObjectId oldTree = git.getRepository().resolve("6525ef3^{tree}");
 		oldTreeIterator.reset(reader, oldTree);
 
 		List<DiffEntry> diffs = git.diff()
 				.setNewTree(newTreeIterator)
 				.setOldTree(oldTreeIterator)
 				.call();
-
+		String[] files = new String[diffs.size()];
+		int i=0;
 		for (DiffEntry diffEntry: diffs){
-			System.out.println(diffEntry);
+			System.out.println(diffEntry.getNewPath());
+			files[i]=diffEntry.getNewPath();
+			files[i]=files[i].replace("src/main/java", "target/classes").replace("src/main/resources", "target/classes").replace(".java", ".class");
+			i++;
 		}
+		
+		zip(files);
 
 	}
+	
+	static final int BUFFER = 2048;
+	public static void zip (String[] files) {
+	      try {
+	         BufferedInputStream origin = null;
+	         FileOutputStream dest = new 
+	           FileOutputStream("e:/workspaces/workspaceTest/updiff/updiff.zip");
+	         ZipOutputStream out = new ZipOutputStream(new 
+	           BufferedOutputStream(dest));
+	         //out.setMethod(ZipOutputStream.DEFLATED);
+	         byte data[] = new byte[BUFFER];
+	         // get a list of files from current directory
+	         File f = new File("e:/workspaces/workspaceTest/updiff/");
+	         
+	         for (int i=0; i<files.length; i++) {
+	            System.out.println("Adding: "+files[i]);
+	            if(files[i].equals(".settings"))
+	            	continue;
+	            FileInputStream fi = new 
+	              FileInputStream(f.getAbsolutePath()+"/"+files[i]);
+	            origin = new 
+	              BufferedInputStream(fi, BUFFER);
+	            files[i]=files[i].replace("/target/", "/WEB-INF/");
+	            
+	            ZipEntry entry = new ZipEntry(files[i]);
+	            out.putNextEntry(entry);
+	            int count;
+	            while((count = origin.read(data, 0, 
+	              BUFFER)) != -1) {
+	               out.write(data, 0, count);
+	            }
+	            origin.close();
+	         }
+	         out.close();
+	      } catch(Exception e) {
+	         e.printStackTrace();
+	      }
+	   }
+
 }
